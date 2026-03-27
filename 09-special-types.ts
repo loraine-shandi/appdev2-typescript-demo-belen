0755b55a26412aa5a8ead9478b7c16926e5463f2
loraine-shandi <shandiloraine@gmail.com>
2026-03-27 15:45:22 +0800

    Done playing and updating the 09-special-types.ts

--- 09-special-types.ts
+++ 09-special-types.ts
@@ -1,25 +1,17 @@
-// Special Type - null
+
 let a: null;
+a = null;
 
-a = null
-// a = "Hi"
+let b: null | string;
+b = null;
+b = "Hello";
 
-let b: null | string
+console.log("b:", b);
 
-b = null
-b = "Hi"
-// b = 10
-
-// Special Type - undefined
 let c: undefined;
+c = undefined;
 
-c = undefined
-// c = null
-// c = "Hi"
+let d: undefined | string;
+d = "World";
 
-let d: undefined | string
-
-d = undefined
-d = "Hi"
-// d = null
-// d = 10
+console.log("d:", d);
