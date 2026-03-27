fdfe0edb0b01b4cc0b8d8f048982a691dd93aab3
loraine-shandi <shandiloraine@gmail.com>
2026-03-27 14:41:36 +0800

    Done playing and updating the 03-flexible-types.ts file

--- 03-flexibile-types.ts
+++ 03-flexibile-types.ts
@@ -1,10 +1,18 @@
-let age: any = 31
+let age: any = 31;
 
-// ...
+// change values
+age = 25;
+console.log(age, typeof age);
 
-age = '31'
-age = true
-age = {}
-age = []
+// experiment with different types
+age = "25";
+console.log(age, typeof age);
 
-console.log(typeof age)
+age = true;
+console.log(age, typeof age);
+
+age = { name: "Dong" };
+console.log(age, typeof age);
+
+age = [1, 2, 3];
+console.log(age, typeof age);
