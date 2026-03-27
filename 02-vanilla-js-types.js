c86182e8928d8737653af8d3fb200e687efd81c1
loraine-shandi <shandiloraine@gmail.com>
2026-03-27 14:35:48 +0800

    Done playing and updating the 02-vanilla-js-types.js

--- 02-vanilla-js-types.js
+++ 02-vanilla-js-types.js
@@ -1,3 +1,14 @@
 let userName1 = "Elmer";
 
+userName1 = "Dong";
+
+console.log(userName1);
 console.log(typeof userName1)
+
+let userAge = 20;
+console.log(userAge);
+console.log(typeof userAge);
+
+userName1 = 12;
+console.log(userName1);
+console.log(typeof userName1);
