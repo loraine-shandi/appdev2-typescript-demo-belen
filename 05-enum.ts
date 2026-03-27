adf03be97fb1cabed82b01df2139a41bfa92698a
loraine-shandi <shandiloraine@gmail.com>
2026-03-27 14:59:28 +0800

    Done playing and updating the 05-enum.ts file

--- 05-enum.ts
+++ 05-enum.ts
@@ -1,14 +1,32 @@
+
 (() => {
     enum Role {
-        // SuperAdmin,
-        // SuperAdmin = 1,
         SuperAdmin = 'SuperAdmin',
-        // Teacher,
         Teacher = 'Teacher',
-        // Student,
         Student = 'Student',
     }
 
-    // let userRole: Role = 0;
-    let userRole: Role = Role.SuperAdmin;
-})
+    // change role
+    let userRole: Role = Role.Student;
+
+    console.log("User role:", userRole);
+
+    // experiment
+    userRole = Role.Teacher;
+    console.log("Updated role:", userRole);
+})();
+
+// (() => {
+//     enum Role {
+//         // SuperAdmin,
+//         // SuperAdmin = 1,
+//         SuperAdmin = 'SuperAdmin',
+//         // Teacher,
+//         Teacher = 'Teacher',
+//         // Student,
+//         Student = 'Student',
+//     }
+
+//     // let userRole: Role = 0;
+//     let userRole: Role = Role.SuperAdmin;
+// })
