ed21df6b63b671c68b37eac5016efa1389988b7e
loraine-shandi <shandiloraine@gmail.com>
2026-04-02 15:15:25 +0800

    Done playing and updating the 10-type-narrowing.ts file

--- 10-type-narrowing.ts
+++ 10-type-narrowing.ts
@@ -1,23 +1,20 @@
-const inputEl = document.getElementById('user-name')
+// Get element
+const inputEl = document.getElementById('user-name') as HTMLInputElement | null;
 
-console.log(inputEl.value) // This line is unsafe because inputEl might be null
+//  unsafe (commented to show experiment)
+// console.log(inputEl.value);
 
+//  Type narrowing (safe check)
 if (!inputEl) {
     throw new Error('Element not found!');
 }
 
-console.log(inputEl.value)
+// Now safe to use
+console.log("Input value:", inputEl.value);
 
-// You can convince TypeScript that you are sure the value will not be null by using the non-null assertion operator `!`
-// You can use the optional chaining `?.` operator to safely check if value is not null before the next operation
+//  Optional chaining (safe access)
+console.log("Optional chaining:", inputEl?.value);
 
-// const inputEl = document.getElementById('user-name')!
-// console.log(inputEl!.value)
-// if (!inputEl) {
-//     throw new Error('Element not found!');
-// }
-// console.log(inputEl?.value)
-
-
-// Type Casting or Assertion
-// const inputEl = document.getElementById('user-name') as HTMLInputElement | null
+//  Non-null assertion (you are sure it exists)
+const inputEl2 = document.getElementById('user-name')! as HTMLInputElement;
+console.log("Non-null assertion:", inputEl2.value);
